13c4a70002904dcea795b24605cc0f8a6422c1a6
willbrid <ngaswilly77@gmail.com>
2026-03-02 14:31:14 +0100

    refactor: inject new logger

--- cmd/main.go
+++ cmd/main.go
@@ -3,31 +3,30 @@ package main
 import (
 	"github.com/willbrid/easy-api-prom-alert-sms/config"
 	"github.com/willbrid/easy-api-prom-alert-sms/internal/app"
-	"github.com/willbrid/easy-api-prom-alert-sms/pkg/logger"
+	"github.com/willbrid/easy-api-prom-alert-sms/pkg/logging"
 
 	"github.com/go-playground/validator/v10"
 )
 
 func main() {
 	validate := validator.New(validator.WithRequiredStructEnabled())
-	loggerInstance := logger.NewLogger()
+	logger := logging.InitLogger()
 
-	configFlag, err := config.LoadConfigFlag(validate)
+	configFlag, err := config.LoadConfigFlag(validate, logger)
 	if err != nil {
-		loggerInstance.Fatal("failed to load configuration flags: %v", err.Error())
+		logger.Error().Err(err).Msg("failed to load configuration flags")
 	}
 
-	viperInstance, err := config.ReadConfigFile(configFlag.ConfigFile)
+	viperInstance, err := config.ReadConfigFile(configFlag.ConfigFile, logger)
 	if err != nil {
-		loggerInstance.Fatal("failed to read configuration file: %v", err.Error())
+		logger.Error().Err(err).Msg("failed to read configuration file")
 	}
 
-	configLoaded, err := config.LoadConfig(viperInstance, validate)
+	configLoaded, err := config.LoadConfig(viperInstance, validate, logger)
 	if err != nil {
-		loggerInstance.Fatal("failed to load configuration file: %v", err.Error())
+		logger.Error().Err(err).Msg("failed to load configuration file")
 	}
 
-	loggerInstance.Info("configuration file '%s' was loaded successfully", configFlag.ConfigFile)
-
-	app.Run(configLoaded, configFlag, loggerInstance)
+	logger.Info().Str("config_file", configFlag.ConfigFile).Msg("configuration file was loaded successfully")
+	app.Run(configLoaded, configFlag, logger)
 }
